0d93ba634404d1b78728d1ecd0f29d1a7c2b6e36
JhonKophler <35379369+JhonKophler@users.noreply.github.com>
2022-09-18 12:14:06 -0300

    realizado punto 5

---
+++ Punto 5 /Program.cs
@@ -0,0 +1,19 @@
+/*Escribe un programa que escriba la tabla de multiplicar de cualquier número entero dado 
+por el usuario, entre 1 y 10.*/
+
+public class HelloWorld
+{
+    public static void tablaDeMultiplicar(int n){
+        Console.WriteLine("TABLA DE " + n + " es:");
+        for(int i = 1; i <= 10 ; i++){
+            Console.WriteLine(i + " X " + n + " = " + i*n);
+        }
+    }
+    
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("INGRESE UN NUMERO:");
+        int userValue = Convert.ToInt32(Console.ReadLine());
+        tablaDeMultiplicar(userValue);
+    }
+}
